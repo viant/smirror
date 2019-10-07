c0d938d7ed2a28ba69f9788ea57d32b9859abe3d
adrianwit@gmail.com <Ubc!1234F>
2019-10-07 09:41:58 -0700

    added topic id support

--- msgbus/pubsub/service.go
+++ msgbus/pubsub/service.go
@@ -6,10 +6,19 @@ import (
 	"fmt"
 	"google.golang.org/api/pubsub/v1"
 	"smirror/msgbus"
+	"strings"
 )
 
 type service struct {
 	*pubsub.Service
+	projectID string
+}
+
+func (s *service) topicInProject(topic string) string {
+	if strings.Count(topic, "/") > 0 {
+		return topic
+	}
+	return fmt.Sprintf("projects/%s/topics/%s", s.projectID, topic)
 }
 
 //Publish publishes data to message bus
@@ -21,12 +30,15 @@ func (s *service) Publish(ctx context.Context, topic string, data []byte, attrib
 			},
 		},
 	}
+
 	if len(attributes) > 0 {
 		request.Messages[0].Attributes = make(map[string]string)
 		for k, v := range attributes {
 			request.Messages[0].Attributes[k] = fmt.Sprintf("%s", v)
 		}
 	}
+
+	topic = s.topicInProject(topic)
 	publish := pubsub.NewProjectsService(s.Service).Topics.Publish(topic, request)
 
 	publish.Context(ctx)
@@ -38,12 +50,13 @@ func (s *service) Publish(ctx context.Context, topic string, data []byte, attrib
 }
 
 //New creates a service
-func New(ctx context.Context) (msgbus.Service, error) {
+func New(ctx context.Context, projectId string) (msgbus.Service, error) {
 	srv, err := pubsub.NewService(ctx)
 	if err != nil {
 		return nil, err
 	}
 	return &service{
+		projectID:projectId,
 		Service: srv,
 	}, nil
 }
