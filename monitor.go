fab80fec14632f5b396fb6c8874c1bb9ec5e2c46
adrianwit@gmail.com <Ubc!1234F>
2019-10-07 16:25:39 -0700

    updated monitor error handling

--- monitor.go
+++ monitor.go
@@ -3,6 +3,7 @@ package smirror
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/pkg/errors"
 	"log"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 
 //StorageMonitor cloud function entry point
 func StorageMonitor(w http.ResponseWriter, r *http.Request) {
+
 	if r.ContentLength > 0 {
 		defer func() {
 			_ = r.Body.Close()
@@ -18,14 +20,22 @@ func StorageMonitor(w http.ResponseWriter, r *http.Request) {
 	}
 	err := checkStorage(w, r)
 	if err != nil {
+		log.Print(err)
+	}
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
 }
 
-func checkStorage(writer http.ResponseWriter, httpRequest *http.Request) error {
+func checkStorage(writer http.ResponseWriter, httpRequest *http.Request) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+		}
+	}()
 	request := &mon.Request{}
-	if err := json.NewDecoder(httpRequest.Body).Decode(&request); err != nil {
+	if err = json.NewDecoder(httpRequest.Body).Decode(&request); err != nil {
 		return errors.Wrapf(err, "failed to decode %T", request)
 	}
 	service, err := mon.NewFromEnv(ConfigEnvKey)
@@ -34,7 +44,7 @@ func checkStorage(writer http.ResponseWriter, httpRequest *http.Request) error {
 	}
 	response := service.Check(context.Background(), request)
 	if err =  json.NewEncoder(writer).Encode(response);err != nil {
-		log.Fatal(err)
+		return err
 	}
-	return nil
+	return err
 }
