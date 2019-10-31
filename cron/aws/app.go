38a8e13325b433ac43a4c3e7b9324b527c8177bc
adrianwit@gmail.com <Ubc!1234F>
2019-10-31 14:27:11 -0700

    updated dep, added loggign

--- cron/aws/app.go
+++ cron/aws/app.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/viant/afsc/s3"
 	"smirror/base"
 	"smirror/cron"
 )
@@ -18,7 +19,12 @@ func main() {
 	lambda.Start(handleRequest)
 }
 
+
+
 func handleRequest(ctx context.Context) (*cron.Response, error) {
+	if base.IsLoggingEnabled() {
+		s3.Logf = s3.StdoutLogger
+	}
 	service, err := cron.NewFromEnv(ctx, base.ConfigEnvKey)
 	if err != nil {
 		return nil, err
